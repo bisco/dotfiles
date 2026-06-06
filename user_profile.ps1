d3119f381b6a29df2d41f94a2cf91a038b971279
bisco <bisco@nbis.co>
2026-06-06 19:40:11 +0900

    add powershell config

---
+++ user_profile.ps1
@@ -0,0 +1,6 @@
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Ctrl+n -Function HistorySearchForward
